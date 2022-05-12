e73c4faba67d97455cc79088d0956805b0637f19
Chateneta <chatenetapro@gmail.com>
2022-05-12 12:17:26 +0200

    add commponents array

--- projects/neumorphy-ui/src/lib/neumorphy-ui.module.ts
+++ projects/neumorphy-ui/src/lib/neumorphy-ui.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { NeumorphyUIComponent } from './neumorphy-ui.component';
 
-
+const COMPONENTS = [];
 
 @NgModule({
   declarations: [
+    ...COMPONENTS,
     NeumorphyUIComponent
   ],
   imports: [
   ],
   exports: [
+    ...COMPONENTS,
     NeumorphyUIComponent
   ]
 })

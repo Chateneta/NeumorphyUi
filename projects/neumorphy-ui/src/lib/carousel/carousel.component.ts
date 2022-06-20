e109b1efee40f46ac8756dafb9d64b5074810f25
Chateneta <chatenetapro@gmail.com>
2022-06-20 14:16:56 +0200

    fix carousel DONE

--- projects/neumorphy-ui/src/lib/carousel/carousel.component.ts
+++ projects/neumorphy-ui/src/lib/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChildren, ElementRef, QueryList, SimpleChange} from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 import { ImageInterface } from './image-interface';
 
 @Component({
@@ -6,29 +6,21 @@ import { ImageInterface } from './image-interface';
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent{
 
   @Input() images: ImageInterface[] = [];
-  @ViewChildren('img') myValue ?:QueryList<ElementRef>;
+  @Input() actualIndex: number = 0;
+  @Output() changeIndex: EventEmitter<number> = new EventEmitter<number>()
 
   constructor() { }
-  ngOnInit(): void {
-    // console.log(this.toto)
-  }
 
   onSplideMoved(args:any){
-    this.myValue?.toArray().forEach(element => {
-      if(element.nativeElement.id == args[0]){
-        element.nativeElement.style.transform = "scale(1.3)";
-      }else{
-        element.nativeElement.style.transform = "scale(1)";
-      }
-    });
+   this.actualIndex = args[0];
+   this.changeIndex.next(this.actualIndex)
   }
-
-  ngOnChanges(args:any){
-    console.log('CarouselChange', args.images.currentValue)
-    this.images = args.images.currentValue
-    console.log('CarouselChange', this.images.slice())
+  ngOnChanges(change:any){
+    if(change.actualIndex){
+      this.actualIndex = change.actualIndex.currentValue
+    }
   }
 }
